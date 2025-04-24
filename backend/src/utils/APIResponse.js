588bb65b2767e23ea87102f0ee1cd5e0c960551a
vivek kachhadiya <ifmdac80@gmail.com>
2025-04-24 19:00:49 +0530

    api response change

--- backend/src/utils/APIResponse.js
+++ backend/src/utils/APIResponse.js
@@ -1,5 +1,6 @@
 class APIResponse extends Response {
     constructor(statusCode , data , message = "Success"){
+        super(message);
         this,statusCode = statusCode
         this.data = data
         this.message = message
